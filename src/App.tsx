e1acc626bd8d73c43bdf1c1217abf10d780397e3
99mac99 <maciekj99@gmail.com>
2022-01-04 19:22:48 +0100

    api update

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,17 @@ import Header from './components/Header/header';
 import { GET_MISSION } from './getMission';
 import SpaceRow from './components/containerRow/Row/row';
 
-
 interface ILaunch {
+	id: string;
 	mission_id: string[];
 	launch_date_utc: number;
 	mission_name: string;
 	description: string[];
-	id: number;
 	found: boolean[] | boolean;
 }
 
 interface MyState {
+	id: string;
 	launches: ILaunch[];
 	missions: { id: string; description: string[] }[];
 	found: boolean[] | boolean;
@@ -33,13 +33,27 @@ function App(): JSX.Element {
 
 	const { data, loading, error } = useQuery<MyState, {}>(GET_MISSION);
 
+	type Data = typeof data;
+	type SortKeys = keyof Data;
+	type SortOrder = 'ascn' | 'desc';
+
+	const [sortKey, setSortKey] = useState<SortKeys>();
+	const [setOrder, setSortOrder] = useState<SortOrder>();
+
+	const headers = [
+		{ key: ' id', label: 'id' },
+		{ key: ' launch_date_utc', label: 'Data' },
+		{ key: 'mission_name', label: 'Nazwa' },
+		{ key: 'description', label: 'Opis' },
+	];
+
 	useEffect(() => {
 		if (data) {
 			const normalizedData = data.launches.map((launch) => {
 				const found = data.missions.map((mission: { id: string }) =>
 					launch.mission_id.includes(mission.id)
 				);
-				// setState(found);
+				setState(found)
 				return { ...launch };
 			});
 			setState(normalizedData);
